91601077c0e2a0230b8af2f7652de3509ad2b665
exzhibit97 <gwizdala.patryk@gmail.com>
2018-03-31 10:39:39 +0200

    initial commit

---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/zad4/Zad4.java
@@ -0,0 +1,16 @@
+package pl.edu.ur.oopl5.zad4;
+import java.util.*;
+
+public class Zad4 {
+    public static void main(String[] args) {
+        Data data1 = new Data();
+        //ustawianie daty
+        data1.ustawDate();
+        //data 7 dni do przodu
+        data1.dataPrzod();
+        //data 7 dni do tyłu
+        data1.dataTyl();
+
+
+    }
+}
